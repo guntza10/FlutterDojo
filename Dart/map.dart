8bbe995bc81bc02ef7323b54619e15f32d7b1fda
guntza10 <jomphop.saibuatong@hotmail.com>
2022-02-18 23:58:20 +0700

    dart tutorial add map

--- Dart/map.dart
+++ Dart/map.dart
@@ -1,5 +1,27 @@
 import 'dart:math';
 
 void main() {
-  
+  // create Map<key,value>
+  Map<String, int> m1 = Map<String, int>(); // empty map
+  print(m1);
+
+  var heroes = {"Superman": "Clark Kent", "Batman": "Bruce Wayne"};
+  heroes["Flash"] = "Barry Allen"; // add another new map data
+  print(heroes);
+  // check map is empty or not
+  print('Empty : ${heroes.isEmpty}');
+  print('NotEmpty : ${heroes.isNotEmpty}');
+  // Length map
+  print('Length : ${heroes.length}');
+  // access key map
+  heroes.keys.forEach((key) => print(key));
+  // access value map
+  heroes.values.forEach((val) => print(val));
+  // add multiple value
+  heroes.addAll({"Lex Luther": "Lex Luther", "Loki": "Loki"});
+  // remove map
+  heroes.remove('Lex Luther');
+  // clear map
+  heroes.clear();
+  print(heroes);
 }
